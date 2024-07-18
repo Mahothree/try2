f4665b12067faeeef8a60e565a8b4df3f17bb3c7
mahothree <mahothree123@gmail.com>
2024-07-19 00:03:57 +0530

    aa

---
+++ Testprgm.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Testprgm {
+	final static double pi=3.14;
+	final static int r=10;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	
+
+				
+					System.out.println("Area of circle= "+(pi*r*r));
+					System.out.println("circumference of circle= "+2*pi*r);
+
+	}
+
+}
